091ab2a289be246ca0c22e7b04aac69d64351879
Bharath Reddy <89402199+Bharath-63@users.noreply.github.com>
2022-04-13 16:00:11 +0530

    Update FOODS_LABELNUTRIENTS_CALORIES.sql

--- models/example/FOODS_LABELNUTRIENTS_CALORIES.sql
+++ models/example/FOODS_LABELNUTRIENTS_CALORIES.sql
@@ -61,7 +61,3 @@ select
 from __dbt__cte__FOODS_LABELNUTRIENTS_CALORIES_AB3
 -- CALORIES at foods/labelNutrients/calories from "DB".AGRI."FOODS_LABELNUTRIENTS"
 where 1 = 1
-
-            ) order by (_AIRBYTE_EMITTED_AT)
-      );
-    alter table "DB".AGRI."FOODS_LABELNUTRIENTS_CALORIES" cluster by (_AIRBYTE_EMITTED_AT);
