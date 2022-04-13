3b8f1f68d8f2330171e0bd3bc33318a34142a28f
Bharath Reddy <89402199+Bharath-63@users.noreply.github.com>
2022-04-13 16:00:48 +0530

    Update FOODS_LABELNUTRIENTS_CARBOHYDRATES.sql

--- models/example/FOODS_LABELNUTRIENTS_CARBOHYDRATES.sql
+++ models/example/FOODS_LABELNUTRIENTS_CARBOHYDRATES.sql
@@ -61,7 +61,3 @@ select
 from __dbt__cte__FOODS_LABELNUTRIENTS_CARBOHYDRATES_AB3
 -- CARBOHYDRATES at foods/labelNutrients/carbohydrates from "DB".AGRI."FOODS_LABELNUTRIENTS"
 where 1 = 1
-
-            ) order by (_AIRBYTE_EMITTED_AT)
-      );
-    alter table "DB".AGRI."FOODS_LABELNUTRIENTS_CARBOHYDRATES" cluster by (_AIRBYTE_EMITTED_AT);
