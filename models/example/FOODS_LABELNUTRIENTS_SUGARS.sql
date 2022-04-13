e2efb728af70aec023c4a296089053edea9f468f
Bharath Reddy <89402199+Bharath-63@users.noreply.github.com>
2022-04-13 16:05:34 +0530

    Update FOODS_LABELNUTRIENTS_SUGARS.sql

--- models/example/FOODS_LABELNUTRIENTS_SUGARS.sql
+++ models/example/FOODS_LABELNUTRIENTS_SUGARS.sql
@@ -61,7 +61,3 @@ select
 from __dbt__cte__FOODS_LABELNUTRIENTS_SUGARS_AB3
 -- SUGARS at foods/labelNutrients/sugars from "DB".AGRI."FOODS_LABELNUTRIENTS"
 where 1 = 1
-
-            ) order by (_AIRBYTE_EMITTED_AT)
-      );
-    alter table "DB".AGRI."FOODS_LABELNUTRIENTS_SUGARS" cluster by (_AIRBYTE_EMITTED_AT);
