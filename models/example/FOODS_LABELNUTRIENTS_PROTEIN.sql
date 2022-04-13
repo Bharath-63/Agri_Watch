10f00c916d8eeb8290bb2bad5afcb61c9ec1c102
Bharath Reddy <89402199+Bharath-63@users.noreply.github.com>
2022-04-13 16:03:55 +0530

    Update FOODS_LABELNUTRIENTS_PROTEIN.sql

--- models/example/FOODS_LABELNUTRIENTS_PROTEIN.sql
+++ models/example/FOODS_LABELNUTRIENTS_PROTEIN.sql
@@ -61,7 +61,3 @@ select
 from __dbt__cte__FOODS_LABELNUTRIENTS_PROTEIN_AB3
 -- PROTEIN at foods/labelNutrients/protein from "DB".AGRI."FOODS_LABELNUTRIENTS"
 where 1 = 1
-
-            ) order by (_AIRBYTE_EMITTED_AT)
-      );
-    alter table "DB".AGRI."FOODS_LABELNUTRIENTS_PROTEIN" cluster by (_AIRBYTE_EMITTED_AT);
