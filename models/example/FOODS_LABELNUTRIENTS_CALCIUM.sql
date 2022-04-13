071940464bc8d109cdf2fabab240df6d1bcb73fb
Bharath Reddy <89402199+Bharath-63@users.noreply.github.com>
2022-04-13 15:59:42 +0530

    Update FOODS_LABELNUTRIENTS_CALCIUM.sql

--- models/example/FOODS_LABELNUTRIENTS_CALCIUM.sql
+++ models/example/FOODS_LABELNUTRIENTS_CALCIUM.sql
@@ -61,7 +61,3 @@ select
 from __dbt__cte__FOODS_LABELNUTRIENTS_CALCIUM_AB3
 -- CALCIUM at foods/labelNutrients/calcium from "DB".AGRI."FOODS_LABELNUTRIENTS"
 where 1 = 1
-
-            ) order by (_AIRBYTE_EMITTED_AT)
-      );
-    alter table "DB".AGRI."FOODS_LABELNUTRIENTS_CALCIUM" cluster by (_AIRBYTE_EMITTED_AT);
